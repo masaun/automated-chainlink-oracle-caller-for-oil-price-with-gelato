e085defd56bcbacdb731ccd2159a2bcf4dd90929
masaun <masanoriuno@gmail.com>
2020-09-13 09:56:03 +0900

    Update /ActionHelloWorld.test.js --> Cannot find module 'ethers'

--- test/ActionHelloWorld.test.js
+++ test/ActionHelloWorld.test.js
@@ -1,7 +1,21 @@
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
 
+/// Gelato
+const GelatoCoreLib = require("@gelatonetwork/core");
+
+/// My contract
 const ActionHelloWorld = artifacts.require("ActionHelloWorld");
+
+
+// The gas limit for our automated CHI.mint TX
+// ActionChiMint caps chiAmount to 140 CHI => 6 mio gas should always suffice
+const SELF_PROVIDER_GAS_LIMIT = 6000000; // 6 mio gas
+
+// This is the gelatoGasPrice that we want to be the trigger for our
+// automatic CHI minting => we set it to half of the initial gas price
+const TRIGGER_GAS_PRICE = GELATO_GAS_PRICE_START.div("2");
+
 
 contract("ActionHelloWorld", function(accounts) {
     /***
@@ -22,6 +36,26 @@ contract("ActionHelloWorld", function(accounts) {
     });
 
     describe("Action via Gelato🍦", () => {
+        it('Call HelloWorld.addNewGreetMessage via UserProxy (Delegatecall)', async () => {
+            const newMessage = 'Hello!!'
+
+            // Specify and Instantiate the Gelato Task
+            taskAutoAddMessageWhenTriggerGasPrice = new GelatoCoreLib.Task({
+              actions: [
+                new GelatoCoreLib.Action({
+                  addr: actionHelloWorld.address,
+                  data: await actionHelloWorld.action(newMessage),
+                  operation: GelatoCoreLib.Operation.Delegatecall,
+                  termsOkCheck: false,
+                }),
+              ],
+              selfProviderGasLimit: SELF_PROVIDER_GAS_LIMIT,
+              // This makes sure we only mint CHI when the gelatoGasPrice is at or below
+              // our desired trigger gas price
+              selfProviderGasPriceCeil: TRIGGER_GAS_PRICE,
+            });
+        });
+
         it('Call HelloWorld.addNewGreetMessage via UserProxy (Delegatecall)', async () => {
             const newMessage = 'Hello!!'
 
