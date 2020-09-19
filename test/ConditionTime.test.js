bd8b0a1e6258dd117cfd4bc8e90dc674b1bc09e7
masaun <masanoriuno@gmail.com>
2020-09-19 09:28:06 +0900

    Add artifacts.require(ConditionTime) into /test

--- test/ConditionTime.test.js
+++ test/ConditionTime.test.js
@@ -1,7 +1,11 @@
 const ethers = require("ethers");
 const GelatoCoreLib = require("@gelatonetwork/core");
 
-const conditionAddress = "0x63129681c487d231aa9148e1e21837165f38deaf"
+/// My contract
+const ConditionTime = artifacts.require("ConditionTime");
+
+const conditionAddress = ConditionTime.address;
+//const conditionAddress = "0x63129681c487d231aa9148e1e21837165f38deaf"
 const conditionAbi = ["function timeCheck(uint256 _timestamp) view returns(string memory)"]
 const iFace = new ethers.utils.Interface(conditionAbi)
 const futureTimestamp = 1599800000
