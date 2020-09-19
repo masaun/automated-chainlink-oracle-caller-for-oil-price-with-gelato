113a23622d7c0ea1e0b22dff0904454c5cdb628e
masaun <masanoriuno@gmail.com>
2020-09-19 10:46:01 +0900

    Add Log

--- test/ConditionTime.test.js
+++ test/ConditionTime.test.js
@@ -6,9 +6,18 @@ const ConditionTime = artifacts.require("ConditionTime");
 
 const conditionAddress = ConditionTime.address;
 //const conditionAddress = "0x63129681c487d231aa9148e1e21837165f38deaf"
-const conditionAbi = ["function timeCheck(uint256 _timestamp) view returns(string memory)"]
+const conditionAbi = ConditionTime.abi;
+//const conditionAbi = ["function timeCheck(uint256 _timestamp) view returns(string memory)"]
 const iFace = new ethers.utils.Interface(conditionAbi)
 const futureTimestamp = 1599800000
+
+
+/// Log
+console.log('=== conditionAddress ===', conditionAddress);
+console.log('=== conditionAbi ===', conditionAbi);
+console.log('=== iFace ===', iFace);
+
+
 
 // #### Create the condition object
 
