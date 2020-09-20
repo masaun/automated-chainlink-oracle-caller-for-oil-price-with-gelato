09054f4c31d5f8df408b8dcebae86617dddccc57
masaun <masanoriuno@gmail.com>
2020-09-20 21:15:07 +0900

    Add OilPriceOracle.sol

---
+++ contracts/OilPriceOracle.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+
+contract OilPriceOracle {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Kovan
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        // If the round is not complete yet, timestamp is 0
+        require(timeStamp > 0, "Round not complete");
+        return price;
+    }
+}
